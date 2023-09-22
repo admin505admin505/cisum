d2c815c7014b4f34dbb77dbd1d56f418c3c0d70e
admin505 <100965441+admin505admin505@users.noreply.github.com>
2023-09-22 11:54:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,75 @@
-import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+$(document).ready(function(){
 
-const playIconContainer = document.getElementById('play-icon');
-let state = 'play';
+  var playlist = [{
+      title:"Hidden",
+      artist:"Miaow",
+      mp3:"http://www.jplayer.org/audio/mp3/Miaow-02-Hidden.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/Miaow-02-Hidden.ogg",
+      poster: "https://i.imgur.com/sCbrzQa.png"
+    },{
+      title:"Cro Magnon Man",
+      artist:"The Stark Palace",
+      mp3:"http://www.jplayer.org/audio/mp3/TSP-01-Cro_magnon_man.mp3",
+      oga:"http://www.jplayer.org/audio/ogg/TSP-01-Cro_magnon_man.ogg",
+      poster: "https://i.imgur.com/lXvsuBu.png"
+    },{
+      title:"Bubble",
+      m4a: "http://www.jplayer.org/audio/m4a/Miaow-07-Bubble.m4a",
+      oga: "http://www.jplayer.org/audio/ogg/Miaow-07-Bubble.ogg",
+      poster: "https://i.imgur.com/klJKSVZ.jpg"
+  }];
 
-const animation = lottieWeb.loadAnimation({
-  container: playIconContainer,
-  path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
-  renderer: 'svg',
-  loop: false,
-  autoplay: false,
-  name: "Demo Animation",
-});
+  var cssSelector = {
+    jPlayer: "#jquery_jplayer",
+    cssSelectorAncestor: ".music-player"
+  };
 
-animation.goToAndStop(14, true);
-
-playIconContainer.addEventListener('click', () => {
-    if(state === 'play') {
-        animation.playSegments([14, 27], true);
-        state = 'pause';
-    } else {
-        animation.playSegments([0, 14], true);
-        state = 'play';
+  var options = {
+    swfPath: "https://cdnjs.cloudflare.com/ajax/libs/jplayer/2.6.4/jquery.jplayer/Jplayer.swf",
+    supplied: "ogv, m4v, oga, mp3",
+    volumechange: function(event) {
+      $( ".volume-level" ).slider("value", event.jPlayer.options.volume);
+    },
+    timeupdate: function(event) {
+      $( ".progress" ).slider("value", event.jPlayer.status.currentPercentAbsolute);
     }
+  };
+
+  var myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
+  var PlayerData = $(cssSelector.jPlayer).data("jPlayer");
+
+
+  // Create the volume slider control
+  $( ".volume-level" ).slider({
+     animate: "fast",
+		max: 1,
+		range: "min",
+		step: 0.01,
+		value : $.jPlayer.prototype.options.volume,
+		slide: function(event, ui) {
+			$(cssSelector.jPlayer).jPlayer("option", "muted", false);
+			$(cssSelector.jPlayer).jPlayer("option", "volume", ui.value);
+		}
+  });
+
+  // Create the progress slider control
+  $( ".progress" ).slider({
+		animate: "fast",
+		max: 100,
+		range: "min",
+		step: 0.1,
+		value : 0,
+		slide: function(event, ui) {
+			var sp = PlayerData.status.seekPercent;
+			if(sp > 0) {
+				// Move the play-head to the value and factor in the seek percent.
+				$(cssSelector.jPlayer).jPlayer("playHead", ui.value * (100 / sp));
+			} else {
+				// Create a timeout to reset this slider to zero.
+				setTimeout(function() {
+					 $( ".progress" ).slider("value", 0);
+				}, 0);
+			}
+		}
+	});
 });
